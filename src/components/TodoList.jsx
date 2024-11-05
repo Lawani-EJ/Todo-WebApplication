a67eadbc0ab486c7a8bc13e7d010c9b85e586434
Lawani-EJ <elyonjohn60@gmail.com>
2024-11-05 12:18:20 +0100

    Adding the UI In adding teh UI needed to show and manage the todo entries and then hook it up to store the values In this commit i have added an input box for the todo items as well as a button to submit

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -3,8 +3,21 @@
 const TodoList = () => {
   return (
     <div>
-      <h2>TODO Application</h2>
-    </div>
+      <h2>TODO</h2>
+      <div>
+                <input
+                    type="text"
+                    placeholder="Enter a task"
+                    required
+                    aria-label="Task text" />
+                <button className="add-button" aria-label="Add task" >
+                    Add
+                </button>
+            </div>
+            <ol id="todo-list">
+                <p>existing tasks will be shown here</p>
+            </ol>
+        </div>
   )
 }
 
